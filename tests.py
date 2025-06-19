c9a1850c6683aee7f04af9443feab891be1b81ef
drakkhenstein <greenknight84@hotmail.com>
2025-06-18 20:16:20 -0400

    Added a run function, tested and debugged

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 # from subdirectory.filename import function_name
-from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 #print(get_file_content("calculator", "lorem.txt"))
@@ -7,6 +7,11 @@ from functions.write_file import write_file
 #print(get_file_content("calculator", "pkg/calculator.py"))
 #print(get_file_content("calculator", "/bin/cat"))
 
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+#print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+#print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+#print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py")) # (this should return an error)
+print(run_python_file("calculator", "nonexistent.py")) # (this should return an error)
